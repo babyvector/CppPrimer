d67d102d9be2ffed8acade05b219db73bc8dc6fd
babyvector <yongkangxuuest@gmail.com>
2018-11-09 13:05:19 +0800

    new jekyll build

--- 10th-section/10-6/10-6/源.cpp
+++ 10th-section/10-6/10-6/源.cpp
@@ -1,3 +1,5 @@
+/*
+
 #include<iostream>
 #include<algorithm>
 #include<vector>
@@ -10,9 +12,13 @@ void print(T input_T){
 		cout << i << endl;
 	}
 }
+bool isShorter(const string& s1,const string& s2){
+	return s1.size() < s2.size();
+}
+
 void elimDups(vector<string>& words){
 	sort(words.begin(),words.end());
-	//按照字典进行排序
+	//先按照字符的个数进行排序，相同长度的字符按照字典进行排序
 	auto end_unique = unique(words.begin(),words.end());
 	//end_unique返回处理过重复字符串之后的字符串数组的尾迭代器
 	words.erase(end_unique,words.end());
@@ -23,9 +29,10 @@ int main(){
 	vector<string>string_container = 
 	{ "the", "quick", "red", "fox", "jumps", "over", "the", "slow","red","title"};
 	elimDups(string_container);
+	stable_sort(string_container.begin(), string_container.end(), isShorter);
 	print(string_container);
 	
 	return 0;
 }
 
-
+*/
